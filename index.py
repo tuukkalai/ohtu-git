333ec95781e63920c084b1b3b1a9e81f131cb5d5
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-01-18 16:30:38 +0200

    Update index from the same place previous master update [7]

--- index.py
+++ index.py
@@ -8,8 +8,8 @@ logger("aloitetaan")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-print(f"{summa(x, y)}")
-print(f"{erotus(x, y)}")
+print(f"Lukujen {x} ja {y} summa on {summa(x, y)}")
+print(f"Lukujen {x} ja {y} erotus on {erotus(x, y)}")
 
 logger("lopetetaan ohjelma")
 print("goodbye!")
